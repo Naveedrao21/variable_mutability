ba94bcbbe10b9b27596d63035d6cecb5d2e058f4
Rao Naveed <oyerao18@gmail.com>
2025-03-11 22:05:32 +0500

    difference between mutable & immutable variables

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
-   let mut  n=5; //mutable variable
+   let mut  n= 5; //mutable variable
    println!("the value of n is:{n}");
-    n=9;
+    n = 9;
    println!("the value of n is:{n}");
+
+let v = 20;  //imutable variable
+println!("the value of v is:{v}");
+// v = 30; //error: cannot assign twice to immutable variable
+println!("the value of v is:{v}");
+
 }
